531931062eb3ca7fd48779116c5647d0441e04dc
Greg Camp (grcamp) <grcamp@cisco.com>
2018-10-17 22:47:27 -0400

    Changed to proper web service

--- ios_office365_acl_creator.py
+++ ios_office365_acl_creator.py
@@ -9,9 +9,18 @@
 import requests
 import logging
 import sys
+import uuid
+import json
 import os
+import datetime
 import argparse
-import xml.etree.ElementTree as ET
+import socket
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+from collections import OrderedDict
 
 # Declare global variables
 logger = logging.getLogger(__name__)
@@ -27,6 +36,53 @@ def fatal(msg):
     logger.fatal(msg)
     exit(1)
 
+def is_ip_address(ipAddress):
+    # Return true if IP is valid, else return false
+    try:
+        socket.inet_aton(ipAddress)
+        return True
+    except socket.error:
+        return False
+
+    # Return False
+    return False
+
+def email_report(smtpServer, subject, body, filename, fromAddr, toAddr, ccAddr=""):
+    logger.info("Emailing {}".format(filename))
+
+    msg = MIMEMultipart()
+    msg['Subject'] = subject
+    msg['From'] = fromAddr
+    msg['To'] = toAddr
+    msg['Cc'] = ccAddr
+
+    rcpt = ccAddr.split(",") + toAddr.split(",")
+
+    msg.attach(MIMEText(body, 'plain'))
+
+    attachment = open(filename, "rb")
+
+    part = MIMEBase('application', 'octet-stream')
+    part.set_payload((attachment).read())
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition', "attachment; filename= {}".format(filename))
+
+    msg.attach(part)
+
+    server = smtplib.SMTP(smtpServer, 25)
+    text = msg.as_string()
+    server.sendmail(fromAddr, rcpt, text)
+    server.quit()
+
+def convert_u_to_str(input):
+    if isinstance(input, dict):
+        return {convert_u_to_str(key): convert_u_to_str(value) for key, value in input.iteritems()}
+    elif isinstance(input, list):
+        return [convert_u_to_str(element) for element in input]
+    elif isinstance(input, unicode):
+        return input.encode('utf-8')
+    else:
+        return input
 
 def cidr_to_netmask(cidr):
     cidr = int(cidr)
@@ -37,12 +93,7 @@ def cidr_to_netmask(cidr):
             str( (0x000000ff & mask)))
 
 def cidr_to_wildcard_mask(cidr):
-    cidr = int(cidr)
-    mask = (0xffffffff >> (32 - cidr)) << (32 - cidr)
-    netmask = str( (0xff000000 & mask) >> 24)   + '.' + \
-              str( (0x00ff0000 & mask) >> 16)   + '.' + \
-              str( (0x0000ff00 & mask) >> 8)    + '.' + \
-              str( (0x000000ff & mask))
+    netmask = cidr_to_netmask(cidr)
 
     wildcard = str(abs(int(netmask.split('.')[0]) - 255)) + '.' + \
                str(abs(int(netmask.split('.')[1]) - 255)) + '.' + \
@@ -51,21 +102,28 @@ def cidr_to_wildcard_mask(cidr):
 
     return wildcard
 
-def get_ios_acl_lines(addresslist, protocol, ports):
+def get_ios_acl_lines(ips, protocol, ports):
     # Declare variables
     lines = []
-    ip_addresses = []
 
-    # permit tcp
-
-    if addresslist.attrib['type'] == 'IPv4':
-        for address in addresslist.iter('address'):
-            for port in ports:
-                line = " permit {} {} {} eq {} any".format(protocol,
-                                                           address.text.split('/')[0],
-                                                           cidr_to_wildcard_mask(address.text.split('/')[1]),
-                                                           port)
+    for ip in ips:
+        if '-' in ports:
+            if is_ip_address(ip.split('/')[0]):
+                line = " permit {} {} {} range {} any".format(protocol,
+                                                           ip.split('/')[0],
+                                                           cidr_to_wildcard_mask(ip.split('/')[1]),
+                                                           ports.replace('-', ' '))
                 lines.append(line)
+        else:
+            port_list = ports.split(',')
+
+            for port in port_list:
+                if is_ip_address(ip.split('/')[0]):
+                    line = " permit {} {} {} eq {} any".format(protocol,
+                                                               ip.split('/')[0],
+                                                               cidr_to_wildcard_mask(ip.split('/')[1]),
+                                                               port)
+                    lines.append(line)
 
     # Return lines
     return lines
@@ -82,20 +140,52 @@ def main(**kwargs):
         args = kwargs
     else:
         parser = argparse.ArgumentParser()
+        parser.add_argument('output_file', help='Output File')
         parser.add_argument('--o365', action='store_true', default=False, help='Office 365 Product Type')
+        parser.add_argument('--email', help='Email File')
         args = parser.parse_args()
 
-    myXML = requests.get('https://support.content.office.net/en-us/static/O365IPAddresses.xml').text
+    # Get WorldWide IP List
+    my_guid = str(uuid.uuid4())
+    my_json = requests.get("https://endpoints.office.com/endpoints/worldwide?clientrequestid={}".format(my_guid)).text
+    items = json.loads(my_json)
+    items = convert_u_to_str(items)
 
-    root = ET.fromstring(myXML)
+    # Loop through each item
+    for item in iter(items):
+        if 'ips' in item and 'tcpPorts' in item:
+            lines += get_ios_acl_lines(item['ips'], 'tcp', item['tcpPorts'])
+        if 'ips' in item and 'udpPorts' in item:
+            lines += get_ios_acl_lines(item['ips'], 'udp', item['udpPorts'])
 
-    for product in root.iter('product'):
-        if (product.attrib['name'] == 'o365') and (args.o365):
-            for addresslist in product.iter('addresslist'):
-                lines += get_ios_acl_lines(addresslist, 'tcp', ['80', '443'])
+    # Remove duplicates
+    uniq_lines = list(OrderedDict.fromkeys(lines))
 
-    # Return None
-    return None
+    with open(args.output_file, 'w') as my_file:
+        for line in uniq_lines:
+            my_file.write("{}\n".format(line))
+
+    if args.email:
+        # Open file
+        json_file = open(args.email, 'r')
+        # Read file into a list
+        config = json.load(json_file)
+        # Close file
+        json_file.close()
+        # Get current Time
+        currentDT = datetime.datetime.now()
+
+
+        # Email report
+        email_report(str(config['email']['smtp']),
+                     "RBS - Office 365 ACL Config - {}/{}/{}".format(str(currentDT.month).zfill(2),
+                                                                     str(currentDT.day).zfill(2),
+                                                                     str(currentDT.year).zfill(2)),
+                     "Office 365 ACL Config File {} attached".format(args.output_file),
+                     args.output_file, str(config['email']['from']), str(config['email']['to']), str(config['email']['cc']))
+
+    # Return 0
+    return 0
 
 
 if __name__ == '__main__':
